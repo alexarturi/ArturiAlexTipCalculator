36df5fdf837dc01cc3209f6c1e7c67e910e90ed2
Alexandre Arturi <alexandrea8@nycstudents.net>
2022-10-04 11:25:31 -0400

    project committ

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class TipCalculator {
     public static void main (String []args){
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); //create the scanner
+
+        //prompting the user for the inputs
         System.out.println("Input the total bill cost: ");
         double bill = scan.nextDouble();
         System.out.println("Input the number of people in the party: ");
@@ -11,20 +13,17 @@ public class TipCalculator {
         double tip = scan.nextDouble();
         double tipDecimal = tip/100; //making it into a decimal to multiply bill by
 
+        //performing calculations on user inputs
         double totalTip = tipDecimal * bill;
         double totalBill = totalTip + bill;
         double tipPerPerson = totalTip/people;
         double totalPerPerson = totalBill/people;
 
-        totalTip = Math.round(totalTip*100)/100.0;
-        totalBill = Math.round(totalBill *100)/100.0;
-        tipPerPerson = Math.round(tipPerPerson*100)/100.0;
-        totalPerPerson = Math.round(totalPerPerson*100)/100.0;
-
-        System.out.println("The total bill is $" + totalBill);
-        System.out.println("The total tip is $" + totalTip);
-        System.out.println("The tip per person is $" + tipPerPerson);
-        System.out.println("The total per person is $" + totalPerPerson);
+        //producing output to user. Also, rounding all numbers to 2 decimal places
+        System.out.println("The total bill is $" + String.format("%.2f", totalBill));
+        System.out.println("The total tip is $" + String.format("%.2f", totalTip));
+        System.out.println("The tip per person is $" + String.format("%.2f", tipPerPerson));
+        System.out.println("The total per person is $" + String.format("%.2f", totalPerPerson));
 
     }
 }
